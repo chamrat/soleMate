615435a8b6d1edf0a5d8975ae0786c7de2c836c3
Chamal Rathnayake <CHAMAL.RATHNAYAKE@baruchmail.cuny.edu>
2018-12-11 18:21:20 -0500

    develop

---
+++ soleMate/WebForm1.aspx.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+
+namespace soleMate
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        string mainConn = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        SqlConnection conn = new SqlConnection(); //object that allows connection
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                countryDropDown();
+                shoeDropDown();
+            }
+
+
+        }
+
+        //Countries retreived from the DBO
+        protected void countryDropDown()
+        {
+
+            try
+            {
+                SqlConnection sqlConn = new SqlConnection(mainConn);
+                sqlConn.Open();
+                SqlCommand sqlcomm = new SqlCommand("select * from [dbo].[countries]", sqlConn);
+                sqlcomm.CommandType = CommandType.Text;
+                CountryDD.DataSource = sqlcomm.ExecuteReader();
+                CountryDD.DataTextField = "country";
+                CountryDD.DataValueField = "countryID";
+                CountryDD.DataBind();
+                CountryDD.Items.Insert(0, new ListItem("--Select Your Country--", "0"));
+                sqlConn.Close();
+            }
+            catch (Exception)
+            {
+
+                errorText.Text = "Error!";
+            }
+
+
+        }
+
+        //States retreived per the country selected
+        protected void CountryDD_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int countryID = Convert.ToInt32(CountryDD.SelectedValue);
+                SqlConnection sqlconn = new SqlConnection(mainConn);
+                sqlconn.Open();
+                SqlCommand sqlcomm = new SqlCommand("select * from [dbo].[states] where countryID =" + countryID, sqlconn);
+                sqlcomm.CommandType = CommandType.Text;
+                stateDD.DataSource = sqlcomm.ExecuteReader();
+                stateDD.DataTextField = "states";
+                stateDD.DataValueField = "stateID";
+                stateDD.DataBind();
+                stateDD.Items.Insert(0, new ListItem("--Select Your State--", "0"));
+            }
+            catch (Exception)
+            {
+                errorText.Text = "Error!";
+            }
+
+
+        }
+
+        //shoe size retreived from the DBO
+        protected void shoeDropDown()
+        {
+            try
+            {
+                SqlConnection sqlConn = new SqlConnection(mainConn);
+                sqlConn.Open();
+                SqlCommand sqlcomm = new SqlCommand("select * from [dbo].[sizes]", sqlConn);
+                sqlcomm.CommandType = CommandType.Text;
+                sizeID.DataSource = sqlcomm.ExecuteReader();
+                sizeID.DataTextField = "size";
+                sizeID.DataValueField = "sizeID";
+                sizeID.DataBind();
+                sizeID.Items.Insert(0, new ListItem("--Select Shoe Size--", "0"));
+                sqlConn.Close();
+
+            }
+            catch (Exception)
+            {
+
+                errorText.Text = "Error!";
+            }
+
+        }
+
+        //Checking Data and Inserting if it's not a duplicate
+        protected void Click_Submit(object sender, EventArgs e)
+        {
+
+            //check the DBO for duplicate entries before storing
+            string connString = "Server=den1.mssql7.gear.host;" + "Initial Catalog=cis4160dbchamal;"
+            + "User ID=cis4160dbchamal;" + "Password=Mk45!7z9?5vR;" + "Integrated Security=false";
+            conn.ConnectionString = connString;
+            conn.Open();
+            //declaring the parameter to check the database for duplicates
+            SqlParameter FNcheck = new SqlParameter("@firstName", SqlDbType.NVarChar);
+            SqlParameter LNcheck = new SqlParameter("@lastName", SqlDbType.NVarChar);
+            SqlParameter EMcheck = new SqlParameter("@email", SqlDbType.NVarChar);
+            try
+            {
+                //attempt to have the check here and call it on click_submit
+                SqlCommand checkUsr = new SqlCommand("SELECT COUNT(*) FROM [dbo].[applicants] " +
+                    "WHERE firstName LIKE @firstName AND lastName LIKE @lastName AND email LIKE @email", conn);
+                checkUsr.Parameters.AddWithValue("@firstName", firstName.Text);
+                checkUsr.Parameters.AddWithValue("@lastName", lastName.Text);
+                checkUsr.Parameters.AddWithValue("@email", email.Text);
+                int firstExist = (int)checkUsr.ExecuteScalar();
+                int lastExist = (int)checkUsr.ExecuteScalar();
+                int emailExist = (int)checkUsr.ExecuteScalar();
+
+                //if the applicant is already in the system update the changed country and state and output that it's already in the system
+                if (firstExist > 0 && lastExist > 0 && emailExist > 0)
+                {
+                    //update applicants shoe information and country and state/province information if they are already in the system
+                    conn = new SqlConnection(connString);
+                    SqlCommand updateRec = new SqlCommand("[dbo].[updateApplicants2]", conn);
+                    updateRec.CommandType = CommandType.StoredProcedure;
+                    updateRec.Parameters.AddWithValue("firstName", firstName.Text);
+                    updateRec.Parameters.AddWithValue("lastName", lastName.Text);
+                    updateRec.Parameters.AddWithValue("email", email.Text);
+                    updateRec.Parameters.AddWithValue("sizeID", sizeID.Text);
+                    updateRec.Parameters.AddWithValue("stateID", stateDD.Text);
+                    updateRec.Parameters.AddWithValue("countryID", CountryDD.Text);
+                    updateRec.Parameters.AddWithValue("leg", legCheck.Text);
+                    conn.Open();
+                    int prompt = updateRec.ExecuteNonQuery();
+                    conn.Close();
+
+                    //show the contacts for matching amputees
+                    conn = new SqlConnection(connString);
+                    SqlCommand findMatch = new SqlCommand("[dbo].[findMatch]", conn);
+                    findMatch.CommandType = CommandType.StoredProcedure;
+                    findMatch.Parameters.AddWithValue("firstName", firstName.Text);
+                    findMatch.Parameters.AddWithValue("lastName", lastName.Text);
+                    findMatch.Parameters.AddWithValue("email", email.Text);
+                    findMatch.Parameters.AddWithValue("sizeID", sizeID.Text);
+                    findMatch.Parameters.AddWithValue("stateID", stateDD.Text);
+                    findMatch.Parameters.AddWithValue("countryID", CountryDD.Text);
+                    findMatch.Parameters.AddWithValue("leg", legCheck.Text);
+                    conn.Open();
+
+                    //print matching amputees for the opposite leg
+                    string title = "";
+                    string strDBResult = " ";
+                    SqlDataReader reader = findMatch.ExecuteReader();
+                    title += "<p> Matching Amputees</p>";
+                    strDBResult += "<ul>";
+
+                    while (reader.Read())
+                    {
+                        strDBResult += "</p>";
+                        strDBResult += "<p>Name: ";
+                        strDBResult += reader[1].ToString();
+                        strDBResult += " ";
+                        strDBResult += reader[2].ToString();
+                        strDBResult += "<p>Email: ";
+                        strDBResult += reader[3].ToString();
+                        strDBResult += " </p>";
+                        strDBResult += "===================";
+                    }
+
+                    strDBResult += "</ul>";
+                    divinPanel.InnerHtml = strDBResult;
+                    conn.Close();
+                    //show a prompt confirming the update of the record
+                    if (prompt != 0)
+
+                    {
+                        showMsg.Text = "Record Updated Succesfully";
+                        showMsg.ForeColor = System.Drawing.Color.CornflowerBlue;
+                        
+                    }
+
+                }
+
+                //if the applicant is new insert the values into the database
+                else
+                {
+                    conn = new SqlConnection(connString);
+                    SqlCommand insertRec = new SqlCommand("[dbo].[insertApplicants]", conn);
+                    insertRec.CommandType = CommandType.StoredProcedure;
+                    insertRec.Parameters.AddWithValue("firstName", firstName.Text);
+                    insertRec.Parameters.AddWithValue("lastName", lastName.Text);
+                    insertRec.Parameters.AddWithValue("email", email.Text);
+                    insertRec.Parameters.AddWithValue("sizeID", sizeID.Text);
+                    insertRec.Parameters.AddWithValue("stateID", stateDD.Text);
+                    insertRec.Parameters.AddWithValue("countryID", CountryDD.Text);
+                    insertRec.Parameters.AddWithValue("leg", legCheck.Text);
+                    conn.Open();
+                    //show a prompt confirming the entry to the applicant
+                    int prompt = insertRec.ExecuteNonQuery();
+                    if (prompt != 0)
+
+                    {
+                        showMsg.Text = "Record Inserted Succesfully into the Database";
+                        showMsg.ForeColor = System.Drawing.Color.CornflowerBlue;
+                        conn.Close();
+                    }
+
+                    //show the contacts for matching amputees
+                    conn = new SqlConnection(connString);
+                    SqlCommand findMatch = new SqlCommand("[dbo].[findMatch]", conn);
+                    findMatch.CommandType = CommandType.StoredProcedure;
+                    findMatch.Parameters.AddWithValue("firstName", firstName.Text);
+                    findMatch.Parameters.AddWithValue("lastName", lastName.Text);
+                    findMatch.Parameters.AddWithValue("email", email.Text);
+                    findMatch.Parameters.AddWithValue("sizeID", sizeID.Text);
+                    findMatch.Parameters.AddWithValue("stateID", stateDD.Text);
+                    findMatch.Parameters.AddWithValue("countryID", CountryDD.Text);
+                    findMatch.Parameters.AddWithValue("leg", legCheck.Text);
+                    conn.Open();
+
+                    //print matching amputees for the opposite leg
+                    string strDBResult = " ";
+                    SqlDataReader reader = findMatch.ExecuteReader();
+                                        
+                    while (reader.Read())
+                    {
+                        strDBResult += "</p>";
+                        strDBResult += "<p>Name: ";
+                        strDBResult += reader[1].ToString();
+                        strDBResult += " ";
+                        strDBResult += reader[2].ToString();                                              
+                        strDBResult += "<p>Email: ";                        
+                        strDBResult += reader[3].ToString();
+                        strDBResult += " </p>";
+                        strDBResult += "===================";
+
+                    }
+                    
+                    divinPanel.InnerHtml = strDBResult;
+                    conn.Close();
+
+
+                }
+
+            }
+
+            catch (Exception)
+            {
+
+                errorText.Text = "Error!";
+            }
+                                                         
+        }
+
+    }
+
+}
---
+++ soleMate/obj/Release/Package/PackageTmp/WebForm1.aspx
@@ -0,0 +1,199 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="soleMate.WebForm1" %>
+
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC" rel="stylesheet">
+    <title>Amputee Coalition: Sole Mate</title>
+    <meta charset="utf-8" />
+    <style>
+        * {
+            font-family: 'Noto Sans TC', sans-serif;
+        }
+
+        header {
+            background-color: #555555;
+            color: aliceblue;
+            height: 300px;
+            padding-top: 5px;
+            width: auto;
+            border-radius: 20px;
+        }
+
+        #wrapper {
+            margin: 0 auto;
+            width: 90%;
+            text-align: center;
+        }
+
+        #left {
+            width: 33.33%;
+            float: left;
+            background-color: #eeefea;
+            height: 550px;
+            font-size: medium;
+            border-radius: 20px;
+        }
+
+        #middle {
+            width: 33.33%;
+            float: left;
+            background-color: #eeefea;
+            height: 550px;
+            font-size: medium;
+            border-radius: 20px;
+        }
+
+        #right {
+            width: 33.33%;
+            float: right;
+            background-color: #fffaf0;
+            height: 550px;
+            font-size: medium;
+            border-radius: 20px;
+        }
+
+        .footer {
+            padding: 2rem;
+            height: auto;
+            background-color: #555555;
+            font-size: medium;
+            text-align: center;
+            border-radius: 20px;
+        }
+
+        .BannerContainer img {
+            width: 100%;
+            height: 200px;
+        }
+
+        .fields {
+            height: 39px;
+        }
+
+        labels.CssClass {
+            text-align: left;
+            height: 39px;
+        }
+
+        .marginFix {
+            text-align: center;
+        }
+
+        .cssfix2 {
+            margin-left: 0px;
+        }
+
+        .err {
+            font-size: 8px
+        }
+
+        .footerTxt {
+            color: aliceblue
+        }
+
+        .errorMessages {
+
+            color:red;
+        }
+        
+    </style>
+
+    <%--favicon for the page--%>
+    <link rel="shortcut icon" type="image/x-icon" href="~/favicon.png" />
+
+</head>
+<body>
+    <div id="wrapper" class="container">
+        <header>
+            <div class="BannerContainer">                
+                <img id="Banner2" src="https://i.imgur.com/qn3uGBF.jpg" />
+            </div>
+            <h1>Shoe Exchange</h1>
+        </header>
+        <form runat="server">
+            <main class="col-lg">
+                <div id="left" class="container">
+
+                    <h4 class="headings">Please Select Your Country and State</h4>
+                    <br />
+                    <asp:Label runat="server" class="labels">Country: </asp:Label>
+                    <br />
+                    <asp:DropDownList runat="server" ID="CountryDD" OnSelectedIndexChanged="CountryDD_SelectedIndexChanged" AutoPostBack="True">
+                    </asp:DropDownList>
+                    <br />
+                    <br />
+                    <asp:Label runat="server" class="labels">State: </asp:Label>
+                    <br />
+                    <asp:DropDownList runat="server" ID="stateDD" AutoPostBack="True">
+                        <asp:ListItem>--Select Your State--</asp:ListItem>
+                    </asp:DropDownList>                    
+             
+                </div>
+                <div id="middle" class="marginFix">
+
+                    <h4 class="headings">Please Enter Your Information</h4>
+                    <br />
+                    <asp:Label runat="server" class="labels">First Name: </asp:Label>
+                    <br />
+                    <asp:TextBox runat="server" ID="firstName" Width="250px"></asp:TextBox>
+                    <br />
+                    <asp:RequiredFieldValidator runat="server" ID="reqName" ControlToValidate="firstName" CssClass="errorMessages" ErrorMessage="Please Enter Your First Name!" />
+                    <br />
+                    <asp:Label runat="server" class="labels">Last Name: </asp:Label>
+                    <br />
+                    <asp:TextBox runat="server" ID="lastName" Width="250px"></asp:TextBox>
+                    <br />
+                    <asp:RequiredFieldValidator runat="server" ID="reqlastName" ControlToValidate="lastName" CssClass="errorMessages" ErrorMessage="Please Enter Your Last Name!" />
+                    <br />
+                    <asp:Label runat="server" class="labels">Email: </asp:Label>
+                    <br />
+                    <asp:TextBox runat="server" ID="email" Width="250px"></asp:TextBox>
+                    <br />
+                    <asp:RequiredFieldValidator runat="server" ID="reEmail" ControlToValidate="email" CssClass="errorMessages" ErrorMessage="Please Enter Your Email!" />
+                    <br />
+                    <asp:Label runat="server" class="labels">Shoe Size: </asp:Label>
+                    <br />
+                    <asp:DropDownList runat="server" ID="sizeID" CssClass="cssfix2" Width="150px"></asp:DropDownList>
+                    <br />
+                    <br />
+                    <asp:Label runat="server" class="labels">Leg: </asp:Label>
+                    <br />
+                    <asp:CheckBoxList runat="server" ID="legCheck" align="center">      
+                        <asp:ListItem>Left</asp:ListItem>
+                        <asp:ListItem>Right</asp:ListItem>
+                    </asp:CheckBoxList>
+                    <br />
+                    <br />
+                    <asp:Button ID="submitButton" Text="Submit" runat="server" OnClick="Click_Submit" />
+
+                </div>
+                <div id="right" class="col-lg">
+
+                    <h4 class="headings">Results</h4>
+                    <p>&nbsp;</p>
+                    <p><i>You can find the name and the email of your match below after you submit the information</i> </p>
+                    <asp:Label class="errorMessages" runat="server" ID="errorText"></asp:Label>
+                    <asp:Label class="errorMessages" runat="server" ID="showMsg"></asp:Label>
+                    <asp:Panel runat="server" ID="aspPanel" ScrollBars="Both" Height="300" Width="100%">
+                        <div id="divinPanel" runat="server">                             
+                             
+                        </div>
+                    </asp:Panel>
+
+                </div>
+
+            </main>
+
+            <div class="footer">
+                <footer>
+                    <p>&nbsp;</p>
+                    <h4 class="footerTxt">CIS 4160: Amuptee Coalition Project</h4>
+                    <h4 class="footerTxt">Copyright © Lewis, Kevin, Chamal </h4>
+                </footer>
+            </div>
+
+        </form>
+    </div>
+</body>
+</html>
